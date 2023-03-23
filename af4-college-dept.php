d306290346982d96181497260962ae473fed0742
AgEButton <89423088+AgEButton@users.noreply.github.com>
2023-03-23 11:05:17 -0400

    Update af4-college-dept.php
    - Override the default single.php template and category template to reroute to custom templates in this plugin
    - Remove the default entry-header for the single.php template so a cover image header can be used

--- af4-college-dept.php
+++ af4-college-dept.php
@@ -3,16 +3,16 @@
  * College Department - AgriFlex4
  *
  * @package      af4-college-dept
- * @author       Zachary Watkins
+ * @author       Zachary Watkins, Elisabeth Button
  * @copyright    2019 Texas A&M AgriLife Communications
  * @license      GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name:  College Department - AgriFlex4
  * Plugin URI:   https://github.com/AgriLife/af4-college-dept
- * Description:  College of Agriculture and Life Sciences Unit variation of the AgriFlex4 theme.
- * Version:      0.1.0
- * Author:       Zachary Watkins
+ * Description:  College of Agriculture and Life Sciences Unit variation of the AgriFlex4 theme. Updated 2022 to reflect new brand guide.
+ * Version:      2.0
+ * Author:       Zachary Watkins, Elisabeth Button
  * Author URI:   https://github.com/ZachWatkins
  * Author Email: zachary.watkins@ag.tamu.edu
  * Text Domain:  af4-college-dept
@@ -58,4 +58,56 @@ function af4_college_dept_activation() {
 		);
 		wp_die( wp_kses_post( $error ) );
 	}
+}
+
+// Override the theme Single template for custom post types
+add_filter( 'single_template', 'override_single_template' );
+function override_single_template( $single_template ){
+    global $post;
+
+    $file = dirname(__FILE__) .'/../../plugins/af4-college-dept/templates/single.php';
+
+    if( file_exists( $file ) ) $single_template = $file;
+
+    return $single_template;
+}
+
+// Override the theme Category template for custom post types 
+add_filter( 'category_template', 'override_category_template' );
+function override_category_template( $category_template ){
+    global $post;
+
+    $file = dirname(__FILE__) .'/../../plugins/af4-college-dept/templates/category-department-updates.php';
+
+    if( file_exists( $file ) ) $category_template = $file;
+
+    return $category_template;
+}
+//* Remove the entry header markup and entry title for posts so they can have a custom header
+
+
+
+add_action( 'genesis_before', 'prefix_remove_entry_header' );
+/**
+ * Remove Entry Header
+ */
+function prefix_remove_entry_header()
+{
+
+	if ( is_page_template( '/plugins/af4-college-dept/templates/single.php' ) ) { return; }
+
+	//* Remove the entry header markup (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+	//* Remove the entry title (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+	//* Remove the entry meta in the entry header (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
+	//* Remove the post format image (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
+
+
 }
